ecfca6e7385058fb5fd2bef4745c6635c6832af2
Kiet Truong <bearcoding288@gmail.com>
2024-05-14 15:04:38 +0700

    fea: add savechange

--- obj/Debug/net7.0/AccountShop.AssemblyInfo.cs
+++ obj/Debug/net7.0/AccountShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccountShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06ce724a647a80722686616ae78a1454c69fb157")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cad288f91cea52e1232b7f8c71f061ebe0e7f06")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccountShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccountShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
