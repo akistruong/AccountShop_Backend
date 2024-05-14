842af90a27ac7699d3590fedb113a4c8693c6ecc
Kiet Truong <bearcoding288@gmail.com>
2024-05-14 15:17:57 +0700

    fea: init paymentmethod controller

---
+++ Areas/Admin/Controllers/PaymentMethod.cs
@@ -0,0 +1,71 @@
+﻿using AccountShop.Helper;
+using AccountShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountShop.Areas.Admin.Controllers
+{
+    [ApiController]
+    [Area("Admin")]
+    [Route("api/[area]/[controller]")]
+    public class PaymentMethod : Controller
+    {
+        AccountShopContext context = DatabaseInstance.GetInstance();
+        [HttpGet]   
+        public IActionResult Index()
+        {
+            try
+            {
+                var methods = context.Paymentmethods;
+                return Ok(methods);
+            }
+            catch (Exception ex)
+            {
+
+                return NotFound();  
+            }
+            return View();
+        }
+        [HttpPost]  
+        public IActionResult Post(Models.Paymentmethod method)
+        {
+            try
+            {
+                var result = context.Paymentmethods.Add(method);
+                context.SaveChanges();
+                return Ok(result);  
+            }catch (Exception ex) { 
+                return NotFound(ex);
+            }
+        }
+        [HttpPut]
+        public IActionResult Put(Models.Paymentmethod method)
+        {
+            try
+            {
+                context.Entry(method).State = EntityState.Modified;
+                context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex);
+            }
+        }
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var payment = context.Paymentmethods.FirstOrDefault(x => x.MethodId == id);
+                var result = context.Paymentmethods.Remove(payment);
+                context.SaveChanges();  
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
--- obj/Debug/net7.0/AccountShop.AssemblyInfo.cs
+++ obj/Debug/net7.0/AccountShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccountShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cad288f91cea52e1232b7f8c71f061ebe0e7f06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fe8a03dba6f644b1d5c52a3e0b3867e0c3d1316")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccountShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccountShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
