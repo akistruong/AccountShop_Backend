8a87f53b55e0f1c61e4a5cd3cf1be074ca1fa74d
Kiet Truong <bearcoding288@gmail.com>
2024-05-14 16:55:54 +0700

    fea:order_controller

--- Areas/Admin/Controllers/Order.cs
+++ Areas/Admin/Controllers/Order.cs
@@ -1,12 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AccountShop.Helper;
+using AccountShop.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AccountShop.Areas.Admin.Controllers
 {
+    [ApiController]
+    [Area("Admin")]
+    [Route("api/[area]/[controller]")]
     public class Order : Controller
     {
-        public IActionResult Index()
+        AccountShopContext context = DatabaseInstance.GetInstance();
+
+        [HttpGet]
+        public IActionResult Get()
         {
-            return View();
+            try
+            {
+                var orders = context.TblOrders;
+                return Ok(orders);
+            }catch (Exception ex) {
+                throw ex;
+            }
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                var order = context.TblOrders.FirstOrDefault(x=>x.OrderId == id);
+                return order!=null? Ok(order):NotFound();
+            }catch (Exception ex) {
+                throw ex;
+            }
+        }
+        [HttpPost]
+        public IActionResult Post(Models.TblOrder order) {
+            try
+            {
+                var result= context.TblOrders.Add(order);
+                context.SaveChanges();
+                return Ok(result);
+            }catch  (Exception ex)
+            {
+                throw ex;
+
+            }
         }
     }
 }
--- obj/Debug/net7.0/AccountShop.AssemblyInfo.cs
+++ obj/Debug/net7.0/AccountShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccountShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1b37b7419fbe6fbf088f7316dc0dc4965c9ff62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f21a968b0dca5b5a81f7a16c5fe0867bb860f60d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccountShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccountShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
