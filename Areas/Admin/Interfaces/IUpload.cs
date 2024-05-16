2767cff352735d3c6f6c1341f05bcf4477139992
Kiet Truong <bearcoding288@gmail.com>
2024-05-16 23:54:37 +0700

    fea: dev upload module

---
+++ Areas/Admin/Interfaces/IUpload.cs
@@ -0,0 +1,8 @@
+﻿namespace AccountShop.Areas.Admin.Interfaces
+{
+    public interface IUpload
+    {
+        public Task<bool> UploadProductImage(IFormFile file,string productID);
+        public Task<bool> UploadProductImages(List<IFormFile> files, string productID);
+    }
+}
