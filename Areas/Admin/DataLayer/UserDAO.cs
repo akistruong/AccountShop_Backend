99eea03de8458522540cd3afb4e23eaa180f030e
Kiet Truong <bearcoding288@gmail.com>
2024-05-18 13:52:01 +0700

    fea: add module user manager

---
+++ Areas/Admin/DataLayer/UserDAO.cs
@@ -0,0 +1,40 @@
+﻿using AccountShop.Helper;
+using AccountShop.Models;
+
+namespace AccountShop.Areas.Admin.DataLayer
+{
+    public class UserDAO
+    {
+        AccountShopContext context = DatabaseInstance.GetInstance();
+        public List<Models.TblUser> Select()
+        { 
+            return context.TblUsers.ToList();   
+        }
+        public Models.TblUser Select(string id)
+        {
+            return context.TblUsers.Find(id)?? new TblUser();   
+        }
+        public Models.TblUser Insert(Models.TblUser user) { 
+            context.TblUsers.Add(user); 
+            context.SaveChanges();  
+            return user;
+        }
+        public Models.TblUser Update(Models.TblUser user) { 
+            context.Entry(user).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            context.SaveChanges();
+            return user;
+            }
+        public bool Delete(string username)
+        {
+            var user = context.TblUsers.Find(username);
+            if(user == null)
+            {
+                return false;
+            }
+            context.TblUsers.Remove(user);
+            context.SaveChanges();
+            return true;
+        }
+
+    }
+}
---
+++ Areas/Admin/Interfaces/IUser.cs
@@ -0,0 +1,12 @@
+﻿namespace AccountShop.Areas.Admin.Interfaces
+{
+    public interface IUser
+    {
+        public List<Models.TblUser> Select();
+        public Models.TblUser Select(string id);
+        public Models.TblUser Insert(Models.TblUser user);
+        public Models.TblUser Update(Models.TblUser user);
+        public bool Delete(string id);
+
+    }
+}
