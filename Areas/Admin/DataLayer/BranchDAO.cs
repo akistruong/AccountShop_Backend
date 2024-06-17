28b4b768b6b4ee97a59e30c1d636956972f78275
Kiet Truong <bearcoding288@gmail.com>
2024-06-17 21:48:43 +0700

    fea:add_IventoryDAO-BranchDAO

---
+++ Areas/Admin/DataLayer/BranchDAO.cs
@@ -0,0 +1,38 @@
+﻿using AccountShop.Models;
+
+namespace AccountShop.Areas.Admin.DataLayer
+{
+    public class BranchDAO
+    {
+
+        AccountShopContext context;
+
+        public BranchDAO(AccountShopContext context)
+        {
+            this.context = context;
+        }
+        public List<Branch> Select() {
+            return context.Branches.ToList();
+        }
+        public Models.Branch Select(int id) {
+            return context.Branches.Find(id);
+        }
+        public Models.Branch Insert(Models.Branch branch) {
+            context.Entry(branch).State = Microsoft.EntityFrameworkCore.EntityState.Added;
+            context.SaveChanges();
+            return branch;
+        }
+        public Models.Branch Update(Models.Branch branch)
+        {
+            context.Entry(branch).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            context.SaveChanges();
+            return branch;
+        }
+        public bool Delete(Models.Branch branch)
+        {
+            context.Entry(branch).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+            context.SaveChanges();
+            return true;
+        }
+    }
+}
